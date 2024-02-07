9fed033902ba33643b247fd20496098e14ce9616
Wangmo Tenzing <tenzing1@llnl.gov>
2024-02-07 00:17:28 -0800

    separating docker build and run into two scripts

--- docker-build.sh
+++ docker-build.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-# Get OUN of user
-OUN=$(whoami)
-
-# Get the UID corresponding to the current username
-USER_ID=$(id -u "$OUN")
-
-docker build --build-arg "OUN=$(whoami)" --build-arg "USER_ID=$(id -u "$OUN")" -t peyeon -f eyeon.Dockerfile .
-docker run -it -v $(pwd):/workdir peyeon /bin/bash
+docker build --build-arg "OUN=$(whoami)" --build-arg "USER_ID=$(id -u $OUN)" -t peyeon -f eyeon.Dockerfile .
