5d89e4d409a31348ad59c823549dd548c93bc4e3
mooney7 <mooney7@llnl.gov>
2024-05-30 13:48:59 -0700

    added tests for checksum.py and its corresponding CLI command

--- src/eyeon/checksum.py
+++ src/eyeon/checksum.py
@@ -1,10 +1,10 @@
 import eyeon.observe as O
 
 
-def Checksum(file:str, hash:str, expected_checksum:str):
+def Checksum(file:str, algorithm:str, expected_checksum:str):
     #Get the md5 hash of the file
-    fileHash= O.Observe.create_hash(file, hash)
-    print(f"hash: {fileHash}")
+    fileHash= O.Observe.create_hash(file, algorithm)
+    print(f"{algorithm} hash: {fileHash}")
     print(f"expected hash: {expected_checksum}")
 
     #compare it to the expected checksum, returns true if match
@@ -12,4 +12,6 @@ def Checksum(file:str, hash:str, expected_checksum:str):
         print("Checksum verification pass")
     else:
         print("Checksum verification fail, double check the expected checksum provided. Otherwise file may have been modified")
+
+    return fileHash
 
