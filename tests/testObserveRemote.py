cf90654f5738980966c9f2e87efe1a679d9c18cc
Wangmo Tenzing <tenzing1@llnl.gov>
2024-06-02 19:22:21 -0700

    remove testValidateJson() with safe_serialize

--- tests/testObserveRemote.py
+++ tests/testObserveRemote.py
@@ -38,12 +38,12 @@ class ObservationTestCase(unittest.TestCase):
         except FileNotFoundError:
             pass
 
-    def testValidateJson(self) -> None:
-        with open("../schema/observation.schema.json") as schem:
-            schema = json.loads(schem.read())
-        vs = vars(self.OBS)
-        obs_json = json.loads(self.OBS._safe_serialize(vs))
-        print(jsonschema.validate(instance=obs_json, schema=schema))
+    # def testValidateJson(self) -> None:
+    #     with open("../schema/observation.schema.json") as schem:
+    #         schema = json.loads(schem.read())
+    #     vs = vars(self.OBS)
+    #     obs_json = json.loads(self.OBS._safe_serialize(vs))
+    #     print(jsonschema.validate(instance=obs_json, schema=schema))
 
     def testConfigJson(self) -> None:
         vs = vars(self.OBS)
