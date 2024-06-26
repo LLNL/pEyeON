38c3e6ff5abb877ff70bdf1bc8b82c85f0add040
Grant Espe <espe1@llnl.gov>
2024-06-26 13:48:31 -0700

    flake8 linter fixes

--- tests/testChecksum.py
+++ tests/testChecksum.py
@@ -8,46 +8,46 @@ from eyeon import checksum
 class testChecksum(unittest.TestCase):
 
     def setUp(self):
-        #Create temp file
-        self.temp_file=tempfile.NamedTemporaryFile(delete=False)
+        # Create temp file
+        self.temp_file = tempfile.NamedTemporaryFile(delete=False)
         self.temp_file.write(b"Test data for hashing.")
         self.temp_file.close()
 
         # Calculate expected hashes
-        self.expected_sha256 ='855e09f327e425f4ce668563db63b3d7e9d3220d1f89aa7b750e5b7722209362'
-        self.expected_md5 ="b208e1c2c00f7ab4775a295a640c0f0f"
-        self.expected_sha1 ="69a2a259cbce0879c21938ecada864898a606613"
+        self.expected_sha256 = "855e09f327e425f4ce668563db63b3d7e9d3220d1f89aa7b750e5b7722209362"
+        self.expected_md5 = "b208e1c2c00f7ab4775a295a640c0f0f"
+        self.expected_sha1 = "69a2a259cbce0879c21938ecada864898a606613"
 
     def tearDown(self):
         # Clean up the temporary file after test
         os.remove(self.temp_file.name)
-    
-    #Check passes
+
+    # Check passes
     def test_hash_file_sha256(self):
-        result = checksum.Checksum(self.temp_file.name, algorithm='sha256', expected_checksum=self.expected_sha256)
+        result = checksum.Checksum(self.temp_file.name, algorithm='sha256', expected_checksum=self.expected_sha256)  # noqa E501
         self.assertEqual(result, self.expected_sha256)
-    
+
     def test_hash_file_md5(self):
-        result = checksum.Checksum(self.temp_file.name, algorithm='md5', expected_checksum=self.expected_md5)
+        result = checksum.Checksum(self.temp_file.name, algorithm='md5', expected_checksum=self.expected_md5)  # noqa E501
         self.assertEqual(result, self.expected_md5)
-    
+
     def test_hash_file_sha1(self):
-        result = checksum.Checksum(self.temp_file.name, algorithm='sha1', expected_checksum=self.expected_sha1)
+        result = checksum.Checksum(self.temp_file.name, algorithm='sha1', expected_checksum=self.expected_sha1)  # noqa E501
         self.assertEqual(result, self.expected_sha1)
 
-
-    #Check fails
+    # Check fails
     def test_hash_file_sha256_fail(self):
-        result = checksum.Checksum(self.temp_file.name, algorithm='sha256', expected_checksum="fj93u2j9ji")
+        result = checksum.Checksum(self.temp_file.name, algorithm='sha256', expected_checksum="fj93u2j9ji")  # noqa E501
         self.assertNotEqual(result, "fj93u2j9ji")
 
     def test_hash_file_md5_fail(self):
-        result = checksum.Checksum(self.temp_file.name, algorithm='md5', expected_checksum="324eqwr2")
+        result = checksum.Checksum(self.temp_file.name, algorithm='md5', expected_checksum="324eqwr2")  # noqa E501
         self.assertNotEqual(result, "324eqwr2")
 
     def test_hash_file_sha1_fail(self):
-        result = checksum.Checksum(self.temp_file.name, algorithm='sha1', expected_checksum="32qreq42")
+        result = checksum.Checksum(self.temp_file.name, algorithm='sha1', expected_checksum="32qreq42")  # noqa E501
         self.assertNotEqual(result, "32qreq42")
+
 
 if __name__ == '__main__':
     unittest.main()
