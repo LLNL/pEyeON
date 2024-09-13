2b23af61df89c1a8afee97bc8dade2cd39780e98
Wangmo Tenzing <tenzing1@llnl.gov>
2024-09-12 17:03:21 -0700

    change DIE to detect it easy

--- src/eyeon/observe.py
+++ src/eyeon/observe.py
@@ -77,7 +77,7 @@ class Observe:
             csv string containing intended install locations
         imphash : str
             Either Import hash for Windows binaries or telfhash for ELF Linux binaries.
-        die : str
+        detect_it_easy : str
             Detect-It-Easy output.
         signatures : dict
             Descriptors of signature information, including signatures and certificates. Only
@@ -100,7 +100,7 @@ class Observe:
         self.bytecount = stat.st_size
         self.filename = os.path.basename(file)  # TODO: split into absolute path maybe?
         self.signatures = []
-        self.set_die(file)
+        self.set_detect_it_easy(file)
         if lief.is_pe(file):
             self.set_imphash(file)
             self.certs = {}
@@ -165,13 +165,13 @@ class Observe:
         pef = pefile.PE(file)
         self.imphash = pef.get_imphash()
 
-    def set_die(self, file: str) -> None:
+    def set_detect_it_easy(self, file: str) -> None:
         """
-        Sets Detect-It-Easy info. WIP
+        Sets Detect-It-Easy info.
         """
         try:
             dp = "/usr/bin"
-            self.die = subprocess.run(
+            self.detect_it_easy = subprocess.run(
                 [os.path.join(dp, "diec"), file], capture_output=True, timeout=10
             ).stdout.decode("utf-8")
         except KeyError:
