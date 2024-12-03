48e432f89dcdb6c58ac3fe5a76f874cc482d33e6
mooney7 <mooney7@llnl.gov>
2024-12-03 13:06:31 -0800

    adjusted parse test to use new binaries. Currently unable to test cert function as none of the binaries produce any

--- tests/testParse.py
+++ tests/testParse.py
@@ -9,60 +9,60 @@ from eyeon import parse
 
 class X86ParseTestCase(unittest.TestCase):
     def checkOutputs(self) -> None:  # these files + paths should be created by parse
-        self.assertTrue(os.path.isdir("./testresults"))
-        self.assertTrue(os.path.isdir("./testresults/certs"))
+        self.assertTrue(os.path.isdir("./results"))
+        self.assertTrue(os.path.isdir("./results/certs"))
         self.assertTrue(
-            os.path.isfile("./testresults/notepad++.exe.0ec33611cb6594903ff88d47c78dcdab.json")
+            os.path.isfile("./results/hello_world.exe.c1550ecc547c89b2f24599c990a29184.json")
         )
         self.assertTrue(
-            os.path.isfile("./testresults/7z_win32.exe.8515170956d36ef9da3082a7c22e8213.json")
+            os.path.isfile("./results/testlib.dll.17b7ca694f2e1e307127c7b653ed294f.json")
         )
 
-    def certExtracted(self) -> None:
-        self.assertTrue(
-            os.path.isfile(
-                "./testresults/certs/46011ede1c147eb2bc731a539b7c047b7ee93e48b9d3c3ba710ce132bbdfac6b.crt"  # noqa: E501
-            )
-        )
-        self.assertTrue(
-            os.path.isfile(
-                "./testresults/certs/866b46dc0876c0b9c85afe6569e49352a021c255c8e7680df6ac1fdbad677033.crt"  # noqa: E501
-            )
-        )  # noqa: E501
+    # def certExtracted(self) -> None:
+    #     self.assertTrue(
+    #         os.path.isfile(
+    #             "./testresults/certs/46011ede1c147eb2bc731a539b7c047b7ee93e48b9d3c3ba710ce132bbdfac6b.crt"  # noqa: E501
+    #         )
+    #     )
+    #     self.assertTrue(
+    #         os.path.isfile(
+    #             "./testresults/certs/866b46dc0876c0b9c85afe6569e49352a021c255c8e7680df6ac1fdbad677033.crt"  # noqa: E501
+    #         )
+    #     )  # noqa: E501
 
-    def validateNotepadJson(self) -> None:
-        with open("./testresults/notepad++.exe.0ec33611cb6594903ff88d47c78dcdab.json") as schem:
+    def validateHelloWorldJson(self) -> None:
+        with open("./results/hello_world.exe.c1550ecc547c89b2f24599c990a29184.json") as schem:
             schema = json.loads(schem.read())
-        self.assertEqual(schema["bytecount"], 6390616)
-        self.assertEqual(schema["filename"], "notepad++.exe")
-        self.assertEqual(schema["md5"], "0ec33611cb6594903ff88d47c78dcdab")
-        self.assertEqual(schema["sha1"], "28a2a37cf2e9550a699b138dddba4b8067c8e1b1")
+        self.assertEqual(schema["bytecount"], 58880)
+        self.assertEqual(schema["filename"], "hello_world.exe")
+        self.assertEqual(schema["md5"], "c1550ecc547c89b2f24599c990a29184")
+        self.assertEqual(schema["sha1"], "e4e8ecba8d39ba23cf6f13498021049d62c3659c")
         self.assertEqual(
-            schema["sha256"], "ccb4ff6b20689d948233807a67d9de9666229625aa6682466ef01917b01ccd3b"
+            schema["sha256"], "de22b757eaa0ba2b79378722e8057d3052edc87caf543b17d8267bd2713162a8"
         )
-        self.assertEqual(schema["authenticode_integrity"], "OK")
-        self.assertEqual(schema["signatures"][0]["verification"], "OK")
-        self.assertEqual(schema["authentihash"], schema["signatures"][0]["sha1"])
+        # self.assertEqual(schema["authenticode_integrity"], "OK")
+        # self.assertEqual(schema["signatures"][0]["verification"], "OK")
+        # self.assertEqual(schema["authentihash"], schema["signatures"][0]["sha1"])
 
-        self.assertNotIn(  # check that the first cert has no issuer in the chain
-            "issuer_sha256", schema["signatures"][0]["certs"][0]
-        )
-        self.assertEqual(  # check that the second cert has the first issuer's sha
-            schema["signatures"][0]["certs"][1]["issuer_sha256"],
-            "46011ede1c147eb2bc731a539b7c047b7ee93e48b9d3c3ba710ce132bbdfac6b",
-        )
+        # self.assertNotIn(  # check that the first cert has no issuer in the chain
+        #     "issuer_sha256", schema["signatures"][0]["certs"][0]
+        # )
+        # self.assertEqual(  # check that the second cert has the first issuer's sha
+        #     schema["signatures"][0]["certs"][1]["issuer_sha256"],
+        #     "46011ede1c147eb2bc731a539b7c047b7ee93e48b9d3c3ba710ce132bbdfac6b",
+        # )
 
-    def validate7zipJson(self) -> None:
-        with open("./testresults/7z_win32.exe.8515170956d36ef9da3082a7c22e8213.json") as schem:
+    def validateTestLibDllJson(self) -> None:
+        with open("./results/testlib.dll.17b7ca694f2e1e307127c7b653ed294f.json") as schem:
             schema = json.loads(schem.read())
-        self.assertEqual(schema["bytecount"], 1330263)
-        self.assertEqual(schema["filename"], "7z_win32.exe")
-        self.assertEqual(schema["md5"], "8515170956d36ef9da3082a7c22e8213")
-        self.assertEqual(schema["sha1"], "66c835bdf217d1ceb2d73f7b8b27d7ccca212b38")
+        self.assertEqual(schema["bytecount"], 53248)
+        self.assertEqual(schema["filename"], "testlib.dll")
+        self.assertEqual(schema["md5"], "17b7ca694f2e1e307127c7b653ed294f")
+        self.assertEqual(schema["sha1"], "77a6248d4bb7f7a58e0868d17057c62d92c9f1c1")
         self.assertEqual(
-            schema["sha256"], "1ea62e6b152e4b7dbadf45289e04bf4ea7431c7928a9b3c6ba5e4c06fe368085"
+            schema["sha256"], "42cc4d90b3348853ce4decc2c8c1142ff26623c53a058630be5bdd2f8d848c00"
         )
-        self.assertFalse(schema["signatures"])  # 7zip has no signatures
+        self.assertFalse(schema["signatures"])  # testLib.dll has no signatures
 
     @classmethod
     def tearDownClass(self) -> None:
@@ -72,14 +72,14 @@ class X86ParseTestCase(unittest.TestCase):
 class X86SinglethreadTestCase(X86ParseTestCase):
     @classmethod
     def setUpClass(self) -> None:
-        self.PRS = parse.Parse("./binaries/x86/", logging.WARNING, "./testParse.log")
+        self.PRS = parse.Parse("./binaries/Windows_dll_test_no1", logging.WARNING, "./testParse.log")
         self.PRS(result_path="testresults")  # run scan
 
     def testCommon(self):
         self.checkOutputs()
-        self.certExtracted()
-        self.validateNotepadJson()
-        self.validate7zipJson()
+        # self.certExtracted()
+        self.validateHelloWorldJson()
+        self.validateTestLibDllJson()
 
     def testLogCreated(self):
         self.assertTrue(os.path.isfile("./testParse.log"))
@@ -93,27 +93,27 @@ class X86SinglethreadTestCase(X86ParseTestCase):
 class X86TwoThreadTestCase(X86ParseTestCase):
     @classmethod
     def setUpClass(self) -> None:
-        self.PRS = parse.Parse("./binaries/x86/")
+        self.PRS = parse.Parse("./binaries/Windows_dll_test_no1")
         self.PRS(result_path="testresults", threads=2)
 
     def testCommon(self):
         self.checkOutputs()
-        self.certExtracted()
-        self.validateNotepadJson()
-        self.validate7zipJson()
+        # self.certExtracted()
+        self.validateHelloWorldJson()
+        self.validateTestLibDllJson()
 
 
 class X86ThreeThreadTestCase(X86ParseTestCase):
     @classmethod
     def setUpClass(self) -> None:
-        self.PRS = parse.Parse("./binaries/x86/")
+        self.PRS = parse.Parse("./binaries/Windows_dll_test_no1")
         self.PRS(result_path="testresults", threads=3)
 
     def testCommon(self):
         self.checkOutputs()
-        self.certExtracted()
-        self.validateNotepadJson()
-        self.validate7zipJson()
+        # self.certExtracted()
+        self.validateHelloWorldJson()
+        self.validateTestLibDllJson()
 
 
 if __name__ == "__main__":
